1a01688eeeec45751203b6eb54d0e3f4c0971e1d
izzkhairable <izzkhair@gmail.com>
2021-10-18 05:06:40 +0800

    Solve bug with formula for packaging charges

--- landShippingCalculator.py
+++ landShippingCalculator.py
@@ -1,15 +1,21 @@
-from shippingCalculator import ShippingCalculator 
+from shippingCalculator import ShippingCalculator
 from countryCharges import CountryCharges
 
-class LandShippingCalculator (ShippingCalculator):
+
+class LandShippingCalculator(ShippingCalculator):
     def __init__(self, custNm, custContact):
-        ShippingCalculator.__init__(self,custNm, custContact)
+        ShippingCalculator.__init__(self, custNm, custContact)
+
     def computeCharges(self):
         base = CountryCharges()
-        baseCharge = base.getBaseCharge(super().getFromCountry(), super().getToCountry())
+        baseCharge = base.getBaseCharge(
+            super().getFromCountry(), super().getToCountry()
+        )
         # Compute Packaging Charges - 10% discount
-        packagingCharge = 1- ((super().getSize()*super().getWeight())*.10)
-        # Compute Custome Charges
-        customCharges =  base.getCustomCharges(super().getToCountry())
-        landFreightCharges = (super().getSize()*0.25) + (super().getWeight()*0.75) + 10
+        packagingCharge = super().getSize() * super().getWeight() * 0.90
+        # Compute Customer Charges
+        customCharges = base.getCustomCharges(super().getToCountry())
+        landFreightCharges = (
+            (super().getSize() * 0.25) + (super().getWeight() * 0.75) + 10
+        )
         return baseCharge + packagingCharge + customCharges + landFreightCharges
