35fc6fb39db28fa0d7827c331b0347e46fbf52ac
izzkhairable <izzkhair@gmail.com>
2021-10-19 15:38:08 +0800

    Refactor code smell dead code

--- contactInformation.py
+++
@@ -1,8 +0,0 @@
-class ContactInformation:
-    def __init__(self, custNm, custContact):
-        self.__customerContact = custContact
-        self.__customerName = custNm
-
-    def setToAddContact(self, toAdd, toCon):
-        self.__toAddress = toAdd
-        self.__toContact = toCon
